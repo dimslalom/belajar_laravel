d3367c7ca414868145a4201e35cda732adbd5368
shugora <sheisasha16@gmail.com>
2023-11-06 14:35:34 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/bootstrap-jumbotron', function () {
+    return view('BOOTSTRAPJUMBOTRON');
+});
+Route::get('/js2', function () {
+    return view('JS2');
+});
+Route::get('/latihan1', function () {
+    return view('Latihan1');
+});
+Route::get('/linktree-hangry', function () {
+    return view('LinktreeHangry');
+});
+Route::get('/tes', function () {
+    return view('tes');
+});
